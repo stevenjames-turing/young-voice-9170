c75e1062e943df69bf38efded6f905718cbecb7d
stevenjames-turing <stevenjamesstl@gmail.com>
2022-02-14 12:57:24 -0600

    Add feature  spec test for average student age

--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -11,9 +11,21 @@ RSpec.describe 'professor show page' do
         ProfessorStudent.create(student_id: longbottom.id, professor_id: snape.id)
 
         visit "/professors/#{snape.id}"
-
+        
         expect(page).to have_content(harry.name)
         expect(page).to have_content(longbottom.name)
         expect(page).to_not have_content(malfoy.name)
+    end
+    
+    it 'displays average age of all students' do 
+        snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+        harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+        longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+        ProfessorStudent.create(student_id: harry.id, professor_id: snape.id)
+        ProfessorStudent.create(student_id: longbottom.id, professor_id: snape.id)
+    
+        visit "/professors/#{snape.id}"
+
+        expect(page).to have_content("Average Age: 11")
     end
 end 
